e3fdbb968abbddb03265edd88d46447318705e49
nekmo <contacto@nekmo.com>
2018-12-06 23:37:51 +0100

    Show error on closed.

--- ansible_keepass.py
+++ ansible_keepass.py
@@ -1,23 +1,54 @@
+import __main__
+import json
+import requests
 from ansible.plugins.vars import BaseVarsPlugin
 from ansible.executor.task_executor import TaskExecutor as _TaskExecutor
 from ansible.executor import task_executor
 from ansible.executor.process import worker
-from keepasshttplib import keepasshttplib
+from keepasshttplib import keepasshttplib, httpclient, encrypter
+from urllib3 import HTTPConnectionPool
 
-k = keepasshttplib.Keepasshttplib()
 
+class Keepass(object):
+    def __init__(self):
+        self.k = keepasshttplib.Keepasshttplib()
 
-def get_password(host):
-    for host_name in [host.name] + [group.name for group in host.groups]:
-        auth = k.get_credentials('ssh://{}'.format(host_name))
-        if auth:
-            return auth[1]
+    def get_password(self, host):
+        if not self.test_connection():
+            print('Keepass is closed! sudo password is not available.')
+            return
+        hosts = [host.name] + [group.name for group in host.groups]
+        for host_name in hosts:
+            auth = self.k.get_credentials('ssh://{}'.format(host_name))
+            if auth:
+                return auth[1]
+        print('The password could not be obtained for {}'.format(', '.join(map(str, hosts))))
+        return
+
+    def test_connection(self):
+        key = self.k.get_key_from_keyring()
+        if key is None:
+            key = encrypter.generate_key()
+        id_ = self.k.get_id_from_keyring()
+        try:
+            return self.k.test_associate(key, id_)
+        except requests.exceptions.ConnectionError:
+            return
+
+    @classmethod
+    def get_or_create_conn(cls):
+        if not getattr(__main__, '_keepass', None):
+            __main__._keepass = Keepass()
+        return __main__._keepass
 
 
 class TaskExecutor(_TaskExecutor):
     def __init__(self, host, task, job_vars, play_context, new_stdin, loader, shared_loader_obj, final_q):
         if play_context.become and not job_vars.get('ansible_become_pass'):
-            job_vars['ansible_become_pass'] = get_password(host)
+            kp = Keepass.get_or_create_conn()
+            password = kp.get_password(host)
+            if password:
+                job_vars['ansible_become_pass'] = password
         super(TaskExecutor, self).__init__(host, task, job_vars, play_context, new_stdin, loader,
                                            shared_loader_obj, final_q)
 
